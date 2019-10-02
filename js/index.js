8ef57245b7dc9a0c9f0bf26ec7e2b24652f15efc
Iliyas Attarwala <iliyasattarwala007@gmail.com>
2019-10-02 18:50:30 +0530

    added portfolio section and javascript code

---
+++ js/index.js
@@ -0,0 +1,44 @@
+var navbar = document.querySelector(".navbar");
+var logo = document.querySelector(".logo");
+var aboutItem = document.getElementById("about-item");
+var portfolioItem = document.getElementById("portfolio-item");
+var contactItem = document.getElementById("contact-item");
+
+
+if(!(/iPhone|iPad|iPod|Android|webOS|BlackBerry|Opera Mini|IEMobile/i.test(navigator.userAgent) )) {
+    window.addEventListener("scroll", debounce(scrollFunction, 10, true));
+    function scrollFunction() {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            navbar.style.background = "#f7f7f7";
+            navbar.style.boxShadow = "0 3px 8px rgba(0,0,0,0.4)";
+            logo.style.background = "#121212";
+            logo.style.color = "#f7f7f7";
+            aboutItem.style.color = "#121212";
+            portfolioItem.style.color = "#121212";
+            contactItem.style.color = "#121212";
+        } else {
+            navbar.style.background = "transparent";
+            navbar.style.boxShadow = "0 0 0 rgba(0,0,0,0.4)";
+            logo.style.background = "#fff";
+            logo.style.color = "#121212";
+            aboutItem.style.color = "rgba(255,255,255,0.7)";
+            portfolioItem.style.color = "rgba(255,255,255,0.7)";
+            contactItem.style.color = "rgba(255,255,255,0.7)";
+        }
+    }
+}
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
