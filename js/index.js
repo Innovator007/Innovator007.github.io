977088acc59ff77b63df208faadcd6823385d474
Iliyas Attarwala <iliyasattarwala007@gmail.com>
2019-10-27 13:23:42 +0530

    intial full design ready for laptop and mobile, tablet left to see

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 (function(){
     emailjs.init('user_gv3cuEmUW8JYnPIABk1zr');
+    //lazy-loading
+    const imgs = document.querySelectorAll('img');
+    const observer = window.lozad(imgs); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
+    observer.observe();
 })();
-
-var navbar = document.querySelector(".navbar");
-var logo = document.querySelector(".logo");
 var aboutItem = document.getElementById("about-item");
 var portfolioItem = document.getElementById("portfolio-item");
 var contactItem = document.getElementById("contact-item");
@@ -19,34 +20,21 @@ function displayContent() {
     mainContent.style.display = "block";
 }
 
-function slideTo(id) {
+function slideTo(id,isInNavigation) {
     var element = document.getElementById(id);
-    window.scrollTo({
-        top: element.offsetTop - 70,
-        behavior: 'smooth'
-    });
-}
-
-if(!(/iPhone|iPad|iPod|Android|webOS|BlackBerry|Opera Mini|IEMobile/i.test(navigator.userAgent) )) {
-    window.addEventListener("scroll", debounce(scrollFunction, 10, true));
-    function scrollFunction() {
-        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-            navbar.style.background = "#f7f7f7";
-            navbar.style.boxShadow = "0 1px 4px rgba(0,0,0,0.4)";
-            logo.style.background = "#121212";
-            logo.style.color = "#f7f7f7";
-            aboutItem.style.color = "#525252";
-            portfolioItem.style.color = "#525252";
-            contactItem.style.color = "#525252";
-        } else {
-            navbar.style.background = "transparent";
-            navbar.style.boxShadow = "0 0 0 rgba(0,0,0,0.4)";
-            logo.style.background = "#fff";
-            logo.style.color = "#121212";
-            aboutItem.style.color = "rgba(255,255,255,0.7)";
-            portfolioItem.style.color = "rgba(255,255,255,0.7)";
-            contactItem.style.color = "rgba(255,255,255,0.7)";
-        }
+    if(isInNavigation) {
+        document.getElementById('nav-toggle').checked = false;
+        setTimeout(function() {
+            window.scrollTo({
+                top: element.offsetTop,
+                behavior: 'smooth'
+            });
+        },700);
+    } else {
+        window.scrollTo({
+            top: element.offsetTop,
+            behavior: 'smooth'
+        });
     }
 }
 
@@ -64,8 +52,6 @@ function debounce(func, wait, immediate) {
 		if (callNow) func.apply(context, args);
 	};
 };
-
-
 
 // Animation
 
